ce57126e0c71d05a0bd5004256bf9cbe1318707f
Mikhail <53335687+mishapolyakov@users.noreply.github.com>
2022-02-25 11:18:45 +0300

    Update script.js
    Rewritten checks for data type, check for division by zero and check for the required operator

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-function Calc ( a, b, operator) {
-    const isValid =
-        operator !== 'undefined' &&
-        typeof(a) === 'number' &&
-        typeof(b) === 'number'
-    if(isValid) {
-            switch (operator) {
-                case '+' :
-                    return a + b
-                case '-' :
-                    return a - b
-                case '*' :
-                    return a * b
-                case '/' :
-                    return a / b
-                default:
-                    return ('unknown operation')
-            }
-        }
-            return 'Error'
+function Calc(num1, num2, operator) {
+    if ( typeof num1 !== "number" || typeof num2 !== "number") {
+        return "this is not a number"
+    }
+    if( num2 === 0 && operator === 'div') {
+        return "fatal Error"
+    }
+    if ( operator !== "sum" && operator !== 'sub' && operator !== 'mult' && operator !== 'div' ) {
+        return "unknow operator"
+    }
+
+    switch (operator) {
+        case 'sum':
+            return num1 + num2
+        case 'sub':
+            return num1 - num2
+        case 'mult':
+            return num1 * num2
+        case 'div':
+            return num1 / num2
+    }
 }
-console.log(Calc(1,2, '+'))
-console.log(Calc('str',5, '-'))
-console.log(Calc(1,0, '*'))
-console.log(Calc(1,5, '/'))
 
-
-
+console.log(Calc(4,5,"sum"))
+console.log(Calc(4,5,"sub"))
+console.log(Calc(4,5,"mult"))
+console.log(Calc(4,5,"div"))
+console.log(Calc(4,0,"div"))
+console.log(Calc(4,5,"+"))
+console.log(Calc(4,"fff","mult"))
